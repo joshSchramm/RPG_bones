a4e96fa342487eaf48eda5466a038f16cf9502d8
Josh <joshSchramm3.4@gmail.com>
2018-01-30 17:18:15 -0800

    Messing around with a main menu
    WIP - found out how to call 'current' selection in script so will add more functionality to buttons later

---
+++ bonesRPG/Assets/0_Project/Scripts/UI/UI_Buttons.cs
@@ -0,0 +1,65 @@
+﻿/* -----------------------------------------------------------------------------------
+ * Class Name: UI_Buttons
+ * -----------------------------------------------------------------------------------
+ * Author: Joshua Schramm
+ * Date Created: 
+ * Last Updated:
+ * -----------------------------------------------------------------------------------
+ * Purpose: 
+ * -----------------------------------------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_Buttons : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+{
+
+    #region VARIABLES
+    
+    #endregion
+
+    #region GETTERS/SETTERS
+
+    #endregion
+
+    /* ------------------------------------------------------------------------------
+    * Function Name: OnPointerEnter
+    * Return types: N/A
+    * Argument types: N/A
+    * Author: 
+    * Date Created: 
+    * Last Updated: 
+    * ------------------------------------------------------------------------------
+    * Purpose: Detects when the mouse hovers over a button
+    * ------------------------------------------------------------------------------
+    */
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Do something.
+        Debug.Log("<color=red>Event:</color> Completed mouse highlight.");
+    }
+
+    /* ------------------------------------------------------------------------------
+    * Function Name: OnSelect
+    * Return types: N/A
+    * Argument types: N/A
+    * Author: 
+    * Date Created: 
+    * Last Updated: 
+    * ------------------------------------------------------------------------------
+    * Purpose: Detects when the button becomes the 'current selection' with other input besides the mouse
+    * ------------------------------------------------------------------------------
+    */
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        // Do something.
+        Debug.Log("<color=red>Event:</color> Completed selection.");
+    }    
+    
+}
+// End UI_Buttons
